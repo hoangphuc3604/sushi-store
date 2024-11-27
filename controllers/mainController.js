c2e83d3b26756832200be1008d48bea9702bd8b8
22120278 <nvhphuc364@gmail.com>
2024-11-27 10:44:53 +0700

    check for role staff

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -4,9 +4,13 @@ const KhachHang = require("../models/khachHangModel");
 class mainController {
     async loadMainPage (req, res) {
         const {email, role} = req;
-        const khachHang = await KhachHang.getKhachHangByEmail(email);
+        if (role === "staff") {
+            res.redirect("/staff");
+            return;
+        }
+        const user = await KhachHang.getKhachHangByEmail(email);
         const allMonAn = await MonAn.getAll();
-        res.render("index", { khachHang, role, allMonAn, title: "Trang Chủ" });
+        res.render("index", { user, role, allMonAn, title: "Trang Chủ" });
     }
 
     async search(req, res) {
@@ -14,9 +18,9 @@ class mainController {
         const {query} = req.body;
         
         const searchResult = await MonAn.searchMonAn(query);
-        const khachHang = await KhachHang.getKhachHangByEmail(email);
+        const user = await KhachHang.getKhachHangByEmail(email);
 
-        res.render("search/search", {query, searchResult, khachHang, title: "Kết quả tìm kiếm"});
+        res.render("search/search", {query, searchResult, user, title: "Kết quả tìm kiếm"});
     }
 }
 
