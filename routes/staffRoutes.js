2c8f6856b8c7314655c539ba60ea24144c19d281
22120278 <nvhphuc364@gmail.com>
2024-11-28 22:16:23 +0700

    add route for stats

--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -8,10 +8,13 @@ router.post("/add-dish", authMiddleware, staffController.postAddDish);
 router.get("/update-dish/:id", authMiddleware, staffController.renderUpdateDishWithId);
 router.post("/update-dish/:id", authMiddleware, staffController.updateDishWithId);
 router.get("/update-dish", authMiddleware, staffController.updateDish);
-router.post("/search/:page", authMiddleware, staffController.search);
+router.post("/search/:page", authMiddleware, staffController.search);  // search dish
 router.get("/booking", authMiddleware, staffController.renderBooking);
 router.post("/booking", authMiddleware, staffController.booking);
 router.get("/statistics/revenue", authMiddleware, staffController.renderRevenueStatistics);
 router.post("/statistics/revenue", authMiddleware, staffController.getRevenueStatistics);
+router.get("/statistics/service", authMiddleware, staffController.renderServiceStatistics)
+router.post("/statistics/service", authMiddleware, staffController.getServiceStatistics)
+router.get("/statistics/employee", authMiddleware, staffController.renderEmployeeStatistics)
 
 module.exports = router;
