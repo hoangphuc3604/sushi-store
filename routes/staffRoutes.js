369bfd7d696156c1fee9941401c4d06f1deb6b7e
22120278 <nvhphuc364@gmail.com>
2024-11-28 10:25:57 +0700

    add soem staff routes

--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -5,7 +5,11 @@ const staffController = require("../controllers/staffController");
 router.get("/", authMiddleware, staffController.loadMainPage);
 router.get("/add-dish", authMiddleware, staffController.addDish);
 router.post("/add-dish", authMiddleware, staffController.postAddDish);
+router.get("/update-dish/:id", authMiddleware, staffController.renderUpdateDishWithId);
+router.post("/update-dish/:id", authMiddleware, staffController.updateDishWithId);
 router.get("/update-dish", authMiddleware, staffController.updateDish);
 router.post("/search/:page", authMiddleware, staffController.search);
+router.get("/booking", authMiddleware, staffController.renderBooking);
+router.post("/booking", authMiddleware, staffController.booking);
 
 module.exports = router;
