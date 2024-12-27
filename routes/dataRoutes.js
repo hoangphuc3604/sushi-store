5a439446bd7b23fea8f141343e557407c5a1c48f
22120278 <nvhphuc364@gmail.com>
2024-12-27 10:46:33 +0700

    refactor dataRoutes for improved readability and consistency

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -1,10 +1,22 @@
-
 const router = require("express").Router();
 const { authMiddleware } = require("../middlewares/authMiddleware");
 const mainController = require("../controllers/mainController");
 
-router.get("/", authMiddleware(["customer", "staff", "admin"]), mainController.loadMainPage);
+router.get(
+  "/",
+  authMiddleware(["customer", "staff", "admin"]),
+  mainController.loadMainPage
+);
 router.post("/search", authMiddleware(["customer"]), mainController.search);
-router.get("/search-result", authMiddleware(["customer"]), mainController.searchResult);
+router.get(
+  "/search-result",
+  authMiddleware(["customer"]),
+  mainController.searchResult
+);
+router.get(
+  "/profile/:id",
+  authMiddleware(["customer"]),
+  mainController.loadProfile
+);
 
 module.exports = router;
