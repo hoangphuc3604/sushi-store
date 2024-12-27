34d4d9f58267ff692349524cbd8d81a2b1368088
22120278 <nvhphuc364@gmail.com>
2024-12-27 10:46:08 +0700

    refactor mainController methods for improved structure and readability

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -2,43 +2,67 @@ const MonAn = require("../models/monAnModel");
 const KhachHang = require("../models/khachHangModel");
 
 class mainController {
-    async loadMainPage (req, res) {
-
-        const {email, role} = req;
-        if (role === "staff") {
-            res.redirect("/staff");
-            return;
-        }
-        if (role === "admin") {
-            res.redirect("/admin");
-            return;
-        }
-
-        const user = await KhachHang.one(email);
-        const allMonAn = await MonAn.all();
-        res.render("index", { user, role, allMonAn, title: "Trang Chủ" });
+  async loadMainPage(req, res) {
+    const { email, role } = req;
+    if (role === "staff") {
+      res.redirect("/staff");
+      return;
+    }
+    if (role === "admin") {
+      res.redirect("/admin");
+      return;
     }
 
-    async search(req, res) {
-        const {email, role} = req;
-        const {query} = req.body;
-        
-        const searchResult = await MonAn.search(query, 0);
-        const user = await KhachHang.one(email);
-        const totalPages = Math.ceil(await MonAn.getSearchLength(query) / 8);
-        const pages = Array.from({length: totalPages}, (_, i) => i + 1);
-        const currentPage = 1;
+    const user = await KhachHang.one(email);
+    const allMonAn = await MonAn.all();
+    res.render("index", { user, role, allMonAn, title: "Trang Chủ" });
+  }
 
-        res.render("search/search", {query, searchResult, user, title: "Kết quả tìm kiếm", role, pages, currentPage, totalPages, prevPage: currentPage - 1, nextPage: currentPage + 1});
-    }
-    
-    async searchResult(req, res) {
-        const {query, page} = req.query;
-        const currentPage = parseInt(page) || 1;
-        const searchResult = await MonAn.search(query, (currentPage - 1));
+  async search(req, res) {
+    const { email, role } = req;
+    const { query } = req.body;
 
-        res.json(searchResult);      
-    }
+    const searchResult = await MonAn.search(query, 0);
+    const user = await KhachHang.one(email);
+    const totalPages = Math.ceil((await MonAn.getSearchLength(query)) / 8);
+    const pages = Array.from({ length: totalPages }, (_, i) => i + 1);
+    const currentPage = 1;
+
+    res.render("search/search", {
+      query,
+      searchResult,
+      user,
+      title: "Kết quả tìm kiếm",
+      role,
+      pages,
+      currentPage,
+      totalPages,
+      prevPage: currentPage - 1,
+      nextPage: currentPage + 1,
+    });
+  }
+
+  async searchResult(req, res) {
+    const { query, page } = req.query;
+    const currentPage = parseInt(page) || 1;
+    const searchResult = await MonAn.search(query, currentPage - 1);
+
+    res.json(searchResult);
+  }
+
+  async loadProfile(req, res) {
+    const { email, role } = req;
+    const { id } = req.params;
+    const user = await KhachHang.one(email);
+
+    const profile = await KhachHang.oneById(id);
+    res.render("profile", {
+      user,
+      role,
+      profile,
+      title: "Thông tin cá nhân",
+    });
+  }
 }
 
 module.exports = new mainController();
